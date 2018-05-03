c49135de103f41f56bdf4bdfdb6f3b98217e145d
Diego Salazar Lazo <dsalazarl@gmail.com>
2018-05-02 21:29:37 -0300

    Se agregan los archivos que faltaban del commit inicial.

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Diego Salazar Lazo\Desktop\Universidad de Chile\Archivos subidos a U-cursos\13.XIIISemestre\IngenieriaDeSoftwareCC4401\Tareas\Tarea3\cc4401_t3_g13\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Diego Salazar Lazo\Desktop\Universidad de Chile\Archivos subidos a U-cursos\13.XIIISemestre\IngenieriaDeSoftwareCC4401\Tareas\Tarea3\cc4401_t3_g13\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==9.0.1','console_scripts','pip3'
+__requires__ = 'pip==9.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==9.0.1', 'console_scripts', 'pip3')()
+    )
