5843b01a741185e198a6f766ced25bf8e532c6c1
maximiliano edwards <maximiliano.edwards@ing.uchile.cl>
2018-05-11 13:29:48 -0300

    add receiver

--- inventario_cei/models.py
+++ inventario_cei/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -59,3 +61,14 @@ class Space(Item):
     )
     condition = models.CharField(max_length=1, choices=CONDITIONS)
     capacity = models.IntegerField()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
