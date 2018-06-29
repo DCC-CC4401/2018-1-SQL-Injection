eac182654bfa0aab00edb5401eb2bcd9942b0234
Jhonny Cerezo <jhoncc2@gmail.com>
2018-06-29 00:46:43 -0400

    template for landing page administrator

--- administrator/views.py
+++ administrator/views.py
@@ -4,7 +4,23 @@ from django.template import loader
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    context = {'message' : "Hello world!"} 
-    template = loader.get_template('administrator/index.html')
+    
+    headers = ['id', 'Usuario', 'Fecha Pres','Fecha Sol']
+    articles = [MockData(), MockData()]
 
+    context = {'message' : "Hello world!",
+                'orderHeaders': headers,
+                'pendingHeaders': headers,
+                'orders': articles,
+                'pending': articles
+            } 
+    template = loader.get_template('administrator/index.html')
+    
     return HttpResponse(template.render(context, request))
+
+
+class MockData():
+    id = 1
+    article = 'Article 1'
+    lendingDate = '27/12/2018'
+    returnDate = '27/12/2019'
