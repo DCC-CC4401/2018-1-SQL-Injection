cd0dd8f885a220dfadf4e52e3a2b29c0b6607257
Diego Salazar Lazo <dsalazarl@gmail.com>
2018-07-02 11:17:08 -0400

    Updated models: Item is now an abstract class and the path of uploaded images was set to folder /img. Updated view of django-admin site.

--- inventario_cei/admin.py
+++ inventario_cei/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Admin, Client
+from .models import Object, Space
+from .models import Reserve
+
+admin.site.site_header = 'Administrador de la Base de Datos del Sistema de Inventario'
+
+admin.site.register(Admin)
+admin.site.register(Client)
+admin.site.register(Space)
+admin.site.register(Object)
+admin.site.register(Reserve)
