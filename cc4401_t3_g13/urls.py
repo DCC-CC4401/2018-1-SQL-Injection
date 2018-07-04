722b9971f424b9a5a6e45bd37f60a42714748b9a
Diego Salazar Lazo <dsalazarl@gmail.com>
2018-07-04 12:13:00 -0400

    Added a simple preliminary list-view of all Items (objects and places) to index view.

--- cc4401_t3_g13/urls.py
+++ cc4401_t3_g13/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('inventario_cei.urls')),
     path('admin/', admin.site.urls),
-    path('administrator/', include('administrator.urls'))
+    path('administrator/', include('administrator.urls')),
 ]
--- inventario_cei/views.py
+++ inventario_cei/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Object, Space
+
+
+def index(request):
+    objects = Object.objects.all()
+    spaces = Space.objects.all()
+    context = {
+        'objects': objects,
+        'spaces': spaces,
+    }
+    template = loader.get_template('index.html')
+
+    return HttpResponse(template.render(context, request))
