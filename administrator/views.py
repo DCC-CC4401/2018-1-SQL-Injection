5ad10b11f51f2f10c8d4ff76b147ad20d67fc91b
Jhonny Cerezo <jhoncc2@gmail.com>
2018-07-05 14:01:45 -0400

    remove mock data for Reservation

--- administrator/views.py
+++ administrator/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
+from inventario_cei.models import Reserve
+
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    
-    headers = ['id', 'Usuario', 'Fecha Pres','Fecha Sol']
-    articles = [MockData(), MockData()]
+    pendingHeaders = ['id', 'usuario', 'space','Fecha Pres', 'Fecha Sol']
+    pending = Reserve.objects.values('user','space','start','finish','id').distinct()
 
     salas = [MockSala(), MockSala(), MockSala(), MockSala()]
 
     context = {'message' : "Hello world!",
-                'orderHeaders': headers,
-                'pendingHeaders': headers,
-                'orders': articles,
-                'pending': articles,
+                'orderHeaders': pendingHeaders,
+                'pendingHeaders': pendingHeaders,
+                'orders': pending,
+                'pending': pending,
                 'salas': salas,
-            } 
+            }
     template = loader.get_template('administrator/index.html')
     
     return HttpResponse(template.render(context, request))
@@ -27,15 +28,18 @@ def index_beta(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
 
     headers = ['id', 'Usuario', 'Fecha Pres', 'Fecha Sol']
-    articles = [MockData(), MockData()]
-
-    salas = [MockSala(), MockSala(), MockSala(), MockSala()]
+    
+    pending = Reserve.objects.all()
+    for a in pending:
+        a.user
+        a.space
+    articles = [MockSala(), MockSala(), MockSala(), MockSala()]
 
     context = {'message': "Hello world!",
                'orderHeaders': headers,
                'pendingHeaders': headers,
                'orders': articles,
-               'pending': articles,
+               'pending': pending,
                'salas': salas,
                }
     template = loader.get_template('base2.html')
