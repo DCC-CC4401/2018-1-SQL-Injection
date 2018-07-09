91c584830981d80d562da027ccd131afe7dfe3e6
Victor <victor.garrido.u@gmail.com>
2018-07-09 19:00:55 -0400

    added advanced search for everything but date

--- inventario_cei/urls.py
+++ inventario_cei/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('list', views.index, name='index'),
     path('index', views.index, name='home'),
     path('espacios', views.spaces, name='espacios'),
     re_path(r'^objetos/$',views.objects, name='search_item')
--- inventario_cei/views.py
+++ inventario_cei/views.py
@@ -13,15 +13,30 @@ def index(request):
 
 
 def objects(request):
-    search_terms = request.GET.get('search_terms')
     made_search = False
+
+    search_terms = request.GET.get('search_terms')
     if search_terms is None:
         items = Object.objects.all()
     else:
         items = Object.objects.filter(name__icontains=search_terms)
         made_search = True
+
+    item_id = request.GET.get('id')
+    if item_id == '':
+        pass
+    else:
+        items = items.filter(id=item_id)
+        made_search = True
+
+    estado = request.GET.get('estado')
+    if estado == 't':
+        pass
+    else:
+        items = items.filter(condition=estado)
+
     context = {
-        'search_terms' : search_terms,
+        'search_terms': search_terms,
         'made_search': made_search,
         'items': items,
         'objects_style': 'btn-success',
