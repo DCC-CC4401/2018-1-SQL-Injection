9c63cf4c70422ab3219f89c120976a4687a222c6
dsalazarl <dsalazarl@gmail.com>
2018-07-10 10:50:20 -0400

    Fixed urls references.

--- inventario_cei/urls.py
+++ inventario_cei/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='home'),
-    path('spaces', views.spaces, name='spaces'),
-    path('espacios', views.spaces, name='espacios'),
+    path('objetos', views.objects, name='search_item'),
+    path('espacios', views.spaces, name='spaces'),
     path('ficha_articulo', views.article_sheet, name='article_sheet'),
 
     # long
@@ -17,5 +17,4 @@ urlpatterns = [
     # helping urls
     path('testdata', views.testdata, name='testdata'),
     path('createtestdata', views.createtestdata, name='createtestdata'),
-    re_path(r'^objetos/$', views.objects, name='search_item')
 ]
